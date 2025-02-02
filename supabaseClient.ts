80ebba30bf5943c9db01fa5e89ee33ced482216b
faizalamat <faizalamat@yahoo.com>
2025-02-02 23:08:18 +0800

    remove manually

--- supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://kondhjkgoyxzwcyudaub.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvbmRoamtnb3l4endjeXVkYXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNTMxMzQsImV4cCI6MjA1MjkyOTEzNH0.vhmelaHHVFwwcItnpv6Trv_e-iGfz4g4ejmY-t6Nttk";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
